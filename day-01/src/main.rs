c7c1eb9528a9728999e9dc73773e243e26ac1b72
Christopher Pence <chris@pencels.net>
2022-12-03 14:19:38 -0800

    day 1

---
+++ day-01/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn get_bundles(path: &str) -> Vec<Vec<i32>> {
+    let contents = fs::read_to_string(path)
+        .expect(&format!("{} not found", path));
+    let mut bundles = Vec::new();
+    let mut bundle = Vec::new();
+
+    for line in contents.lines() {
+        if line == "" {
+            bundles.push(bundle);
+            bundle = Vec::new();
+        } else {
+            let calories: i32 = line.parse().unwrap();
+            bundle.push(calories);
+        }
+    }
+
+    bundles
+}
+
+fn sum(slice: &[i32]) -> i32 {
+    let mut s = 0;
+    for i in slice {
+        s += i;
+    }
+    s
+}
+
+fn main() {
+    let bundles = get_bundles("input.txt");
+    let mut max = 0;
+    for bundle in &bundles {
+        max = max.max(sum(&bundle));
+    }
+    println!("max is {}", max);
+}
