2181d7d908af68ea8876d75124f3efc0dfb5027d
Christopher Pence <chris@pencels.net>
2022-12-06 19:59:14 -0800

    (day 6) readability

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -2,10 +2,14 @@ use std::{collections::HashSet, fs};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+fn is_distinct(window: &[u8]) -> bool {
+    window.iter().collect::<HashSet<_>>().len() == window.len()
+}
+
 fn find_marker_start(sequence: &[u8], size: usize) -> usize {
     size + sequence
         .windows(size)
-        .take_while(|window| window.iter().collect::<HashSet<_>>().len() != size)
+        .take_while(|window| !is_distinct(window))
         .count()
 }
 
